fe64c3b8091a959a62c817dd84b17e6e7604823f
rabizao <rabizao@gmail.com>
2020-04-29 03:40:29 -0300

    integracao backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +11,7 @@ db = SQLAlchemy(app) # render_as_batch=True to allow delete columns while in sql
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+CORS(app)
 
 from app.api import bp as api_bp
 app.register_blueprint(api_bp, url_prefix='/api')
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api'
 
 import './styles.css';
 
@@ -14,24 +16,51 @@ import Text from '../../components/Text';
 import Image from '../../components/Image';
 
 export default function Login() {
-    return(
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        const auth = (
+            { 
+                auth: 
+                {
+                    username:username, 
+                    password:password
+                }
+            }
+        );
+
+        try {
+            const response = await api.post('tokens', {}, auth);
+            alert(`Token de acesso: ${response.data.token}`);
+        } catch {
+            alert('Erro ao requisitar token');
+        }
+    }
+    return (
         <div>
-            
             <Flyer background="white">
-                
                 <Box>
-                <Image src={logoOrangeImg} size="big" alt="indicAI"/>
-                <Title color="black">Faça seu login</Title>
-                
-                    
+                    <Image src={logoOrangeImg} size="big" alt="indicAI" />
+                    <Title color="black">Faça seu login</Title>
                 </Box>
                 <Box>
-                    <form>
-                        <input placeholder="Nome de usuario"/>
-                        <input type="password" placeholder="Senha"/>
+                    <form onSubmit={handleLogin}>
+                        <input
+                            placeholder="Nome de usuario"
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Senha"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
                         <button type="submit" className="button">Entrar</button>
                         <Link to="/register"><Text size="small">Não tenho cadastro</Text></Link>
-                        
                     </form>
                 </Box>
             </Flyer>
