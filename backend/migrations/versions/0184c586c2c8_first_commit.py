be43194dc86c06bb40774e857138ed9e3a588050
Rafael Bizao <rabizao@gmail.com>
2020-05-10 18:37:03 -0300

    db fix

--- backend/migrations/versions/0184c586c2c8_first_commit.py
+++ backend/migrations/versions/0184c586c2c8_first_commit.py
@@ -1,8 +1,8 @@
-"""first
+"""first commit
 
-Revision ID: fd3f5dc9359c
+Revision ID: 0184c586c2c8
 Revises: 
-Create Date: 2020-04-18 18:12:42.774713
+Create Date: 2020-05-10 18:31:12.380564
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'fd3f5dc9359c'
+revision = '0184c586c2c8'
 down_revision = None
 branch_labels = None
 depends_on = None
